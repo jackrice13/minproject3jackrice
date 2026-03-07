8cd20b42909ac614d75e60f667b7edf5f1ced42f
jackrice13 <jackrice13@gmail.com>
2026-03-07 10:09:18 -0500

    Commiting init and file directories

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    return "<p>Index Page</p>"
+
+
+@app.route("/hello")
 def hello_world():
     return "<p>Hello, World!</p>"
+
